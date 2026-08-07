c9a21bb7c8a7ee156c4d79ce14f05407742bfcfe
an-aegis <john@mareimbrium.org>
2026-08-07 13:52:53 +0200

    Update code to a full demo of V0.5
    Functionality:
    - E/O rank promotion/demoting
    - Error handling and reporting
    - Protections against promoting ones self
    - Protections against illegal promotions

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 import discord
 from discord.ext import commands
+from VNDFHelper import *
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,118 +18,13 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!BANNANAR", intents=intents) # the prefix is silly because my IDE generates warnings if its set to None
 
-rankval_id = {
-    "E-0": 1298370746371801118,
-    
-    "E-1": 1225563199118774272,
-    
-    "E-2": 1225563291888128020,
-    
-    "E-3": 1225563457668120586,
-    
-    "E-4": 1225563531907436635,
-    
-    "E-5": 1225563853757218858,
-    
-    "E-6": 1225564296772321281,
-    
-    "E-7": 1225564368201060412,
-    
-    "E-8": 1235143664016031785,
-    
-    "E-9": 1235143924448759819,
-    
-    "E-10": 1235144094620323890,
-    
-    "O-1": 1225816033751863467,
-    
-    "O-2": 1235144296856948816,
-    
-    "O-3": 1235144506316423288,
-    
-    "O-4": 1235144644032200734,
-    
-    "O-5": 1235144876103045191,
-    
-    "O-6": 1225776709908041749,
-    
-    "O-7": 1235151827331190784,
-    
-    "O-8": 1235152116193038428,
-    
-    "O-9": 1225564174764081202,
-    
-    "O-10": 1238572551651856394,
-    }
-
-def findname(rankval: str): # this could be a dict, but i felt like a function was prettier
-    match rankval:
-        case "E-0":
-            return "Recruit"
-        case "E-1":
-            return "PSC"
-        case "E-2":
-            return "PFC"
-        case "E-3":
-            return "Specialist"
-        case "E-4":
-            return "Corporal"
-        case "E-5":
-            return "Sergeant"
-        case "E-6":
-            return "Staff Sergeant"
-        case "E-7":
-            return "Sgnt Mj. B"
-        case "E-8":
-            return "Sgnt Mj. A"
-        case "E-9":
-            return "First Sergeant"
-        case "E-10":
-            return "Command Sergeant"
-        case "O-1":
-            return "Lieutenant"
-        case "O-2":
-            return "Captain"
-        case "O-3":
-            return "Major"
-        case "O-4":
-            return "Colonel 2nd Cl."
-        case "O-5":
-            return "Colonel 1st Cl."
-        case "O-6":
-            return "Brigadier General"
-        case "O-7":
-            return "Lieutenant General"
-        case "O-8":
-            return "Major General"
-        case "O-9":
-            return "General 1st Cl."
-        case "O-10":
-            return "Field Marshal"
-
-async def get_role(member,ID:int):
-    role = member.guild.get_role(ID)
-    return role
-
-def hunt_for_rankval(roles:list) -> str:
-    id_rankval = {value: (value, key) for key,value in rankval_id.items()} # shush ik its slow
-    
-    for role in roles:
-        if role.id in id_rankval:
-            break
-    
-    #try:
-    return id_rankval[role.id]
-    #except Exception as e:
-    #    raise RuntimeError(e)
-
 class rank:
     def __init__(self, who = None, roles = None, rankval = "", __rankname = ""):
-        self.who = who # discord member object
-        self.roles = roles
-        self.rankval = rankval
-        self.rankname = __rankname
-        self.displayname = ""
+        self.who:discord.Member = who # discord member object
+        self.roles:list = roles
+        self.rankval:str = rankval
+        self.rankname:str = __rankname
+        self.displayname:str|None = ""
         
         if self.who != None:
             self.displayname = self.who.nick
@@ -138,42 +34,77 @@ class rank:
                 self.roles = self.who.roles
             
             self.rankval = hunt_for_rankval(self.roles)
+            # error generating function is intended to create unhandled exceptions in this class
+            # this is so the caller can handle this exception
         
         self.rankname = findname(self.rankval)
     
-    def promo_calc(self, by:int) -> int:
+    def promo_calc(self, by:int, limit:int) -> int:
         """
-returns int or None (if fail)
+returns int or generates error
 
 will return int id of the new rank, if it is unable to do this (if rank exceeds the limit),
 will raise an error
 """
-        try:
-            ranklist = list(rankval_id)
-            newrank = ranklist.index(self.rankval)+by
-            return rankval_id[newrank]
-        except:
-            raise IndexError
+
+        ranklist = list(rankval_id)
+        
+        newrank_index = ranklist.index(self.rankval)+by
+        
+        if newrank_index < limit:
+            newrankval = ranklist[newrank_index] # find new rankval
+            return rankval_id[newrankval]
+        else:
+            raise IndexError("The rank you are trying to promote to is above your authority level or is impossible")
     
-    async def promote(self,by:int):
+    async def promote(self,by:int, limit:int):
         old = await get_role(self.who, rankval_id[self.rankval])
-        await self.who.remove_roles(old)
         
-        try:
-            new_id = self.promo_calc(by)
-            await self.who.add_roles(await get_role(self.who, new_id))
-        except:
-            raise RuntimeError("Promotion cannot promote to an imaginary rank")
+        new_id = self.promo_calc(by,limit)
+        # error generating function is intended to create unhandled exceptions in this class
+        # this is so the caller can handle this exception
+        # exception also serves to protect from disalowed promotion cases
         
+        await self.who.add_roles(await get_role(self.who, new_id))
+        await self.who.remove_roles(old) # prevent removal without assignment first
         
 
 @bot.tree.command(name = "promote", description = "promote a user by N ranks")
 async def promote(interaction: discord.Interaction, who:discord.Member, by:int):
-    #try:
-    await rank(who).promote(by)
-    await interaction.response.send_message(f"{who.nick} promoted by {by} ranks", ephemeral=True)
-    #except Exception as e:
-        #await interaction.response.send_message(f"Error:\n{e}", ephemeral=True)
+    C2 = 1534948457704329266
+    C3 = 1534948456572125365
+    C4 = 1534948455041073265
+    FM = 1534960058813648906-1 # first marshal
+    caller_roles = interaction.user.roles # interaction.user is secretly member
+    
+    # it is important to note that get_role uses the first argument to get the user guild
+    # it does not matter if the user actually has the role
+    if await get_role(who, FM) in caller_roles:
+        limit = 20 # O-10
+    elif await get_role(who, C2) in caller_roles:
+        limit = 1 # E-1
+    elif await get_role(who, C3) in caller_roles:
+        limit = 9 # E-8
+    elif await get_role(who, C4) in caller_roles:
+        limit = 17 # O-6
+    else:
+        await interaction.response.send_message(f"You (<@{interaction.user.id}>) have no known promotion authority", ephemeral=True)
+        return
+    
+    if interaction.user.id == who.id:
+        await interaction.response.send_message("You can't promote yourself", ephemeral=True)
+        return
+    
+    target = bot.get_channel(1534948751213330542) # sorry about the magical number
+    try:
+        await rank(who).promote(by, limit)
+        await interaction.response.send_message(f"{who.nick} promoted by {by} ranks", ephemeral=True)
+        
+        await target.send(f"<@{who.id}> was {'promoted' if by > 0 else 'demoted'} by {abs(by)} ranks\nPromoter: <@{interaction.user.id}>")
+        
+    except Exception as e:
+        await interaction.response.send_message(f"Error: `{e}`", ephemeral=True)
+        await target.send(f"<@{who.id}> had a failed promotion/demotion from user <@{interaction.user.id}>\nError: `{e}`")
 
 @bot.event
 async def on_ready():
